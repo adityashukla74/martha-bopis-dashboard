082defd3e6dda5c60e61043e9a58c7dc7e941793
Aditya Shukla <adityashukla74@gmail.com>
2022-07-18 21:28:04 +0530

    added phone number of customer in env variables

--- pages/api/sendsms.js
+++ pages/api/sendsms.js
@@ -1,25 +1,25 @@
 const client = require("twilio")(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_AUTH_TOKEN
-  );
-  
-  const sendmessage = async (req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-  
-    client.messages
-      .create({
-        from: process.env.TWILIO_FROM_PHONE_NUMBER,
-        to: req.body.to,
-        body: req.body.body,
-      })
-      .then(() => {
-        res.send(JSON.stringify({ success: true }));
-      })
-      .catch((err) => {
-        console.log(err);
-        res.send(JSON.stringify({ success: false }));
-      });
-  };
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
-  export default sendmessage;
+const sendmessage = async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+
+  client.messages
+    .create({
+      from: process.env.TWILIO_FROM_PHONE_NUMBER,
+      to: process.env.TWILIO_TO_CUSTOMER,
+      body: req.body.body,
+    })
+    .then(() => {
+      res.send(JSON.stringify({ success: true }));
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(JSON.stringify({ success: false }));
+    });
+};
+
+export default sendmessage;
